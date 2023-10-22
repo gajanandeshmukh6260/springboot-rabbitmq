d111f7f339465e0aedcfabb251571e2ac4be21cd
Gajanan Deshmukh <gajanandeshmukh6260@gmail.com>
2023-10-22 17:35:03 +0530

    create spring boot rabbitmq

---
+++ src/main/java/com/example/springbootrabbitmq/controller/MessageJsonController.java
@@ -0,0 +1,28 @@
+package com.example.springbootrabbitmq.controller;
+
+import com.example.springbootrabbitmq.dto.User;
+import com.example.springbootrabbitmq.publisher.RabbitMQJsonProducer;
+import com.example.springbootrabbitmq.publisher.RabbitMQProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MessageJsonController {
+
+    private RabbitMQJsonProducer jsonProducer;
+
+    public MessageJsonController(RabbitMQJsonProducer jsonProducer) {
+        this.jsonProducer = jsonProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> sendJsonMessage(@RequestBody User user){
+        jsonProducer.sendJsonMessage(user);
+        return ResponseEntity.ok("Json message sent to RabbitMQ ...");
+    }
+
+}
